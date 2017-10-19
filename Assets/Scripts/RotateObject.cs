a881b2e2eea931003b7d37711fb38663fb49959e
KacobJeith <jacobkeith42@gmail.com>
2017-10-19 14:22:35 -0400

    Initial Commit

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+	public float speed = 3;
+	void Update () {
+		
+		transform.Rotate (Time.deltaTime * speed, Time.deltaTime * speed, 0);
+	}
+}
